deedfae9825a6c4c00d27d184632e5c034b5f4ee
Kaleigh Spurio <kaleighastell@gmail.com>
2020-08-03 14:30:23 -0400

    added datalist to the input

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -9,16 +9,26 @@ export default class AppContainer extends React.Component {
     results: Data,
   };
 
+//   set the filter in the state to what is entered in the input
   handleInputChange = (event) => {
     const search = event.target.name;
     const value = event.target.value;
     this.setState({ [search]: value });
   }
 
+//   TODO: change the alert to something with bootstrap- toast?
   handleFormSubmit = (event) => {
     event.preventDefault();
     const filteredResults = this.state.results.filter(employee => employee.role === this.state.filter)
-    this.setState({results: filteredResults});
+    console.log(filteredResults.length)
+    // if there are results that match the filter, set the state.results to the new results
+    // if no results, a message appears
+    if (filteredResults.length !== 0) {
+        this.setState({results: filteredResults});
+    } else {
+        alert('Oops, look like there was a typo or there are no roles that match your search.')
+    }
+    
   }
 
   handleTableReset = (event) => {
@@ -29,7 +39,7 @@ export default class AppContainer extends React.Component {
   render() {
     return (
       <div className='container' >
-        <Filter handleFormSubmit={this.handleFormSubmit} handleInputChange={this.handleInputChange} filter={this.state.filter} />
+        <Filter handleFormSubmit={this.handleFormSubmit} handleInputChange={this.handleInputChange} filter={this.state.filter} data={this.state.results} />
         <Table data={this.state.results} />
       </div>
     );
--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,13 +1,25 @@
 import React from 'react';
-import Data from './Employees.json';
 
 export default function Filter(props) {
+    // lets give options of possible filters on the input...
+    const roleArray = []
+    props.data.forEach(entry => {
+        roleArray.push(entry.role)
+    })
+    // This makes an array of only unique roles
+    // credit for this small code snippet: https://stackoverflow.com/questions/1960473/get-all-unique-values-in-a-javascript-array-remove-duplicates
+    const uniqueRoles = Array.from(new Set(roleArray))
 
     return (
-      <form className="search container mb-4">
+      <form className="search container mb-4 col-8">
         <div className="form-inline ">
           <label className='col-4' htmlFor="filter">Filter by role</label>
-          <input name='filter' type='text' onChange={props.handleInputChange} value={props.filter} className='form-control' placeholder='Enter a role to filter by' id='filter'/>
+          <input name='filter' list='roles' type='text' onChange={props.handleInputChange} value={props.filter} className='form-control col-6' placeholder='Enter a role to filter by' id='filter'/>
+          <datalist id='roles'>
+              {uniqueRoles.map(role => (
+                  <option value={role} key={role} />
+              ))}
+              </datalist>
           <button type="submit" className='btn btn-success m-2' onClick={props.handleFormSubmit} >Filter</button>
         </div>
       </form>
