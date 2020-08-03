3819645d475b32c940fb2270f82b347fb9fe3ac9
Kaleigh Spurio <kaleighastell@gmail.com>
2020-08-03 12:51:53 -0400

    set up the filter form select box

---
+++ src/components/Filter.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Data from './Employees.json';
+
+export default class Filter extends React.Component {
+  state = {
+    filter: '',
+  };
+
+  render() {
+    return (
+      <form className="search">
+        <div className="form-group">
+          <label htmlFor="filter">Filter by role</label>
+          <select className="form-control" id="filter" name="filter">
+            <option value={Data[0].role} key={Data[0].id}>
+              {Data[0].role}
+            </option>
+            <option value={Data[1].role} key={Data[1].id}>
+              {Data[1].role}
+            </option>
+            <option value={Data[3].role} key={Data[3].id}>
+              {Data[3].role}
+            </option>
+          </select>
+        </div>
+      </form>
+    );
+  }
+}
